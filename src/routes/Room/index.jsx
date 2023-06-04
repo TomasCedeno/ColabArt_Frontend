5cc9a9b64a2f9f77eaac4a370462df818e36ed8a
TomasCedeno <tomasj.cedenop@gmail.com>
2023-06-04 12:21:22 -0500

    Added draw menu

--- src/routes/Room/index.jsx
+++ src/routes/Room/index.jsx
@@ -12,10 +12,56 @@ const Room = () => {
     return <div className="room">
         <Navbar />
 
-        <Canvas 
-            canvasRef={canvasRef}
-            ctx={ctx}
-        />
+        <div className="draw">
+            <Canvas 
+                canvasRef={canvasRef}
+                ctx={ctx}
+            />
+            
+            <div className="draw-menu">
+                <ul>
+                    <li>
+                        <div className='btn color'>
+                            <i className="fi fi-rr-palette"></i>
+                            <input type="color" />
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='btn tools'>
+                            <i className="fi fi-rs-resources"></i>
+                            <select name="tool" id="tool">
+                                <option value="pencil">Lápiz</option>
+                                <option value="rectangle">Rectángulo</option>
+                                <option value="circle">Círculo</option>
+                            </select>
+                        </div>
+                    </li>
+
+                    <li className='thick'>
+                        <div className='btn'>
+                            <i className="fi fi-rr-line-width"></i>
+                        </div>
+                        <div className="thickness">
+                            <label htmlFor="thickness">Grosor: {5}</label>
+                            <input type="range" min={1} max={20} name='thickness' />
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='btn erase'>
+                            <i className="fi fi-rs-eraser"></i>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className='btn share'>
+                            <i className="fi fi-rs-share"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 }
 
